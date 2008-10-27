4b6d97389f5c48a82f3f1b9384deef3148f6fe44
Mathieu Boespflug <mboes@lix.polytechnique.fr>
2008-10-27 23:32:57 +0100

    Transition to using ByteString in Directory.hs, do the parsing using PCRE regexps, rename 'parse*' functions to 'pa_*', implement the 'many' parser combinator.

--- Codec/MIME/ContentType/Text/Directory.hs
+++ Codec/MIME/ContentType/Text/Directory.hs
@@ -1,14 +1,15 @@
 module Codec.MIME.ContentType.Text.Directory
     ( Directory, Property(..), Type(..), Parameter(..), Value(..)
     , Rfc2425Types
-    , nakedType, (@@), parseDirectory
-    , parseTextList ) where
+    , nakedType, (@@)) where
 
 import Data.Time
-import Data.Bits (shiftL)
-import Data.Char (toLower, chr, digitToInt, isAscii, isHexDigit)
-import Data.List (intersperse)
 import System.Locale
+import Data.Char (toLower)
+import Data.Maybe (fromJust)
+import Text.Regex.PCRE.ByteString.Lazy
+import qualified Data.ByteString.Lazy.Char8 as B
+import System.IO.Unsafe
 
 
 type Directory u = [Property u]
@@ -20,34 +21,34 @@ data Property u = Prop
                   deriving Show
 
 data Type = Type
-    { type_group :: Maybe String
-    , type_name :: String }
+    { type_group :: Maybe B.ByteString
+    , type_name :: B.ByteString }
             deriving Show
 
 instance Eq Type where
-    x == y = let f = map toLower . type_name
+    x == y = let f = B.map toLower . type_name
              in f x == f y
 
 -- | Make a property type without any grouping.
-nakedType :: String -> Type
+nakedType :: B.ByteString -> Type
 nakedType name = Type { type_group = Nothing, type_name = name }
 
-(@@) :: Property u -> String -> Bool
+(@@) :: Property u -> B.ByteString -> Bool
 p @@ name = prop_type p == nakedType name
 
 instance Ord Type where
-    compare x y = let f = map toLower . type_name
+    compare x y = let f = B.map toLower . type_name
                   in compare (f x) (f y)
 
 data Parameter = Param
-    { param_name :: String
-    , param_value :: String }
+    { param_name :: B.ByteString
+    , param_value :: B.ByteString }
                  deriving Show
 
-type URI = String
+type URI = B.ByteString
 
 data Value u = URI URI
-             | Text String
+             | Text B.ByteString
              | Date Day
              | Time DiffTime
              | DateTime UTCTime
@@ -66,110 +67,133 @@ data Rfc2425Types
 
 -- | Break the input into logical lines, unfolding lines that span
 -- multiple physical lines.
-unfoldLines :: String -> [String]
+unfoldLines :: B.ByteString -> [B.ByteString]
 unfoldLines "" = []
 unfoldLines s =
-    let (l, s') = break (== '\o15') s
-    in case s' of
+    let (l, s') = B.break (== '\o15') s
+        join = case unfoldLines (B.drop 3 s') of
+              [] -> [l]
+              cont:rest -> (B.append l cont) : rest
+    in case B.unpack s' of
          [] -> [l]
-         '\o15':'\o12':' ':s'' -> case unfoldLines s'' of
-                                [] -> [l]
-                                cont:rest -> (l ++ cont) : rest
-         '\o15':'\o12':s'' -> l : unfoldLines s''
+         '\r':'\n':' ':_ -> join
+         '\r':'\n':'\t':_ -> join
+         '\r':'\n':_ -> l : unfoldLines (B.drop 2 s')
          _ -> error "Malformed input: no LF after a CR."
 
-parseDirectory
-    :: ((Type, [Parameter]) -> String -> [Value u])
-    -- ^ Given a Property Type and a list of parameters, parse a string
-    -- representation into a Value.
-    -> String
-    -> Directory u
-parseDirectory valparse = concatMap (parseProperty valparse) . unfoldLines
+newtype P a = P { unP :: B.ByteString -> (a, B.ByteString) }
 
--- | Parse a string representation into a property. Note that the
+instance Monad P where
+    return x = P $ \s -> (x, s)
+    m >>= k = P $ \s -> let (a, s') = unP m s in unP (k a) s'
+
+p :: B.ByteString   -- ^ Text of the regular expression.
+  -> P B.ByteString -- ^ The matching part of the input.
+p pat = P $ \s -> unsafePerformIO $ do
+          Right r <- compile compUngreedy execAnchored pat
+          Right result <- regexec r s
+          return $ case result of
+                     Just (_, match, s', _) -> (match, s')
+                     Nothing -> error "Parse error."
+
+capture :: B.ByteString     -- ^ Text of the regular expression containing capturing groups.
+        -> P [B.ByteString] -- ^ The captured subparts of the input.
+capture pat = P $ \s -> unsafePerformIO $ do
+                Right r <- compile compUngreedy execAnchored pat
+                Right result <- regexec r s
+                return $ case result of
+                           Just (_, _, s', captures) -> (captures, s')
+                           Nothing -> error "Parse error."
+
+parseDirectory :: (Type -> [Parameter] -> B.ByteString -> [Value u])
+               -- ^ Given a Property Type and a list of parameters,
+               -- parse a string representation into a Value.
+               -> B.ByteString
+               -> Directory u
+parseDirectory valparse = concatMap (fst . unP (pa_property valparse)) . unfoldLines
+
+-- | Pa_ a string representation into a property. Note that the
 -- return type here is actually a list of properties, because we
 -- desugar properties whose values are lists into a list of
 -- properties, one for each element of the value list.
-parseProperty
-    :: ((Type, [Parameter]) -> String -> [Value u])
-    -- ^ Given a Property Type and a list of parameters, parse a string
-    -- representation into a (list of) Value.
-    -> String
-    -> [Property u]
-parseProperty valparse line =
-    let (beg, rest) = break (\x -> x == ';' || x == ':') line
-        name = parseType beg
-        (params, ':':value) = parseParameterListS rest
-    in map (\val -> Prop { prop_type = name
-                         , prop_parameters = params
-                         , prop_value = val }) (valparse (name, params) value)
+pa_property :: (Type -> [Parameter] -> B.ByteString -> [Value u])
+              -- ^ Given a Property Type and a list of parameters,
+              -- parse a string representation into a (list of) Value.
+              -> P [Property u]
+pa_property valparse = do
+  [groupt, typt, sept] <- capture "(?:((?:[[:alnum:]]|-)+).)?((?:[[:alnum:]]|-)+)(:|;)"
+  params <- case B.unpack sept of
+              ";" -> pa_parameterList
+              ":" -> return []
+  rest <- p ".*$"
+  let group = if B.null groupt then Nothing else Just groupt
+  let typ = Type { type_group = group, type_name = typt }
+      prop v = Prop { prop_type = typ
+                    , prop_parameters = params
+                    , prop_value = v }
+  return $ map prop $ valparse typ params rest
 
-parseParameterListS :: String -> ([Parameter], String)
-parseParameterListS (':':xs) = ([], ':':xs)
-parseParameterListS (';':':':xs) = ([], ':':xs)
-parseParameterListS (';':xs) = parseParameterListS xs
-parseParameterListS xs =
-    let (param, rest) = parseParameterS xs
-        (params, rest') = parseParameterListS rest
-    in (param:params, rest')
-
-parseParameterS :: String -> (Parameter, String)
-parseParameterS xs =
-    case break (== '=') xs of
-      (name, _:'"':rest) | not (null name) ->
-          let (val, _:rest') = break (== '"') rest
-              param = Param { param_name = name, param_value = val }
-          in (param, rest')
-      (name,_:rest) | not (null name) ->
-          let (val, rest') = break (\x -> x == ',' || x == ':') rest
-              param = Param { param_name = name, param_value = val }
-          in case rest' of
-               ',':rest' -> (param, rest')
-               _ -> (param, rest')
-      _ -> error "Illegal parameter definition."
-
-
-parseType :: String -> Type
-parseType xs = case break (== '.') xs of
-                 (a, []) -> Type { type_group = Nothing, type_name = a }
-                 (a, _:b)  -> Type { type_group = Just a, type_name = b }
+pa_parameterList :: P [Parameter]
+pa_parameterList = do
+  [name, val, qval, sep] <- capture "((?:[[:alnum:]]|-)+)=(?:([^;:,\"]*)|\"([^\"]*)\")(,|:)"
+  ps <- case sep of
+          "," -> pa_parameterList
+          ":" -> return []
+  let value = if B.null val then qval else val
+  return $ Param { param_name = name, param_value = value } : ps
 
 -- A few canned parsers for value types defined in rfc2425
 
-parseURI :: String -> URI
-parseURI = id
+pa_URI :: B.ByteString -> Value u
+pa_URI = Text
 
 -- | Unescape slashes, newlines and commas.
-parseText :: String -> String
-parseText = head . parseTextList
+pa_text :: B.ByteString -> Value u
+pa_text = Text . head . pa_textList
 
-parseDate :: String -> UTCTime
-parseDate = readTime defaultTimeLocale (iso8601DateFormat Nothing)
+pa_date :: B.ByteString -> Value u
+pa_date =
+    Date . readTime defaultTimeLocale (iso8601DateFormat Nothing) . B.unpack
 
-parseTime :: String -> DiffTime
-parseTime = utctDayTime . readTime defaultTimeLocale "%T"
+pa_time :: B.ByteString -> Value u
+pa_time = Time . utctDayTime . readTime defaultTimeLocale "%T" . B.unpack
 
-parseDateTime :: String -> Day
-parseDateTime = readTime defaultTimeLocale (iso8601DateFormat (Just "T%T"))
+pa_dateTime :: B.ByteString -> Value u
+pa_dateTime =
+    DateTime .
+    readTime defaultTimeLocale (iso8601DateFormat (Just "T%T")) .
+    B.unpack
 
-parseInteger :: String -> Integer
-parseInteger = read
+pa_integer :: B.ByteString -> Value u
+pa_integer = Integer . fst . fromJust . B.readInteger
 
-parseBool :: String -> Bool
-parseBool = read
+pa_bool :: B.ByteString -> Value u
+pa_bool "TRUE" = Boolean True
+pa_bool "FALSE" = Boolean False
 
-parseFloat :: String -> Float
-parseFloat = read
+pa_float :: B.ByteString -> Value u
+pa_float = Float . read . B.unpack
 
-parseTextList :: String -> [String]
-parseTextList "" = []
-parseTextList xs = foldr f [[]] xs
-    where f ','  (xs:xss) = []:xs:xss
-          f '\\' ([]:xs:xss) = (',':xs):xss
-          f '\\' (('n':xs):xss) = ('\n':xs):xss
-          f '\\' (('N':xs):xss) = ('\n':xs):xss
-          f '\\' (('\\':xs):xss) = ('\\':xs):xss
-          f x (xs:xss) = (x:xs):xss
+pa_textList :: B.ByteString -> [B.ByteString]
+pa_textList "" = []
+pa_textList xs = B.foldr f [B.empty] xs
+    where f :: Char -> [B.ByteString] -> [B.ByteString]
+          f ','  (xs:xss) = B.empty:xs:xss
+          f '\\' ("":xs:xss) = (B.cons ',' xs):xss
+          f '\\' (xs:xss) | Just ('n',_)  <- B.uncons xs = B.cons '\n' xs : xss
+          f '\\' (xs:xss) | Just ('N',_)  <- B.uncons xs = B.cons '\n' xs : xss
+          f '\\' (xs:xss) | Just ('\\',_) <- B.uncons xs = B.cons '\\' xs : xss
+          f x (xs:xss) = B.cons x xs : xss
 
-parseList :: (String -> a) -> [a]
-parseList = undefined
+-- | Take a parser for single values to a parser for a list of values. This
+-- assumes that the separator between values is the "," character.
+many :: (B.ByteString -> Value u) -> B.ByteString -> [Value u]
+many pa input = map pa $ breakAll input
+    where breakAll "" = []
+          breakAll xs = ys : breakAll (B.drop 1 zs)
+              where (ys, zs) = B.span (/= ',') xs
+
+-- | Convenience function to turn makes a parser for a single value return a
+-- singleton list.
+single :: (B.ByteString -> Value u) -> B.ByteString -> [Value u]
+single pa = (:[]) . pa
