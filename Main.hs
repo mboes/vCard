f7b63891b84f07970aaadeb6e1035f57ab211862
Mathieu Boespflug <mboes@lix.polytechnique.fr>
2008-10-28 17:56:00 +0100

    Stub Main.hs.

---
+++ Main.hs
@@ -0,0 +1,20 @@
+module Main where
+
+import qualified Data.ByteString.Lazy as B
+import Text.VCard.Parser
+import Text.VCard.PrettyPrint
+import System.IO
+import System
+
+printUsage = putStrLn "vcard2ldif [FILE]" >> exitFailure
+
+main = do
+  args <- getArgs
+  (filename, handle) <-
+      case args of
+        [file] -> openFile file ReadMode >>= return . ((,) file)
+        [] -> return ("<stdin>", stdin)
+        _ -> printUsage
+  input <- B.hGetContents handle
+  mapM_ ppVCard $ parseVCards filename input
+
