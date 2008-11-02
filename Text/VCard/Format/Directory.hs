16cb03af62847d0fa7884b30789c4bd7b6270ab3
Mathieu Boespflug <mboes@lix.polytechnique.fr>
2008-11-02 22:31:13 +0100

    Parse values according to property parameters and property types. Dependence on base64-string removed since decoding/encoding is now done uniformly by mime-directory.

--- Text/VCard/Format/Directory.hs
+++ Text/VCard/Format/Directory.hs
@@ -3,7 +3,6 @@ module Text.VCard.Format.Directory
 
 import Text.VCard
 import qualified Codec.MIME.ContentType.Text.Directory as D
-import qualified Codec.Binary.Base64.String as Base64
 import qualified Data.Map as Map
 import qualified Data.ByteString.Lazy.Char8 as B
 import Text.Printf
@@ -13,7 +12,7 @@ import Data.List (intercalate)
 instance D.PrintValue ExtraValue where
     printValue (Sequence xs) =
         D.escape ",;" $ B.intercalate "," $ intercalate [";"] xs
-    printValue (Binary blob) = B.pack $ Base64.encode $ B.unpack blob
+    printValue (Binary blob) = blob
     printValue (PhoneNumber num) = num
     printValue (UTCOffset sign hrs mins) =
         B.pack $ printf "%c%02d:%02d" sign hrs mins
@@ -34,7 +33,7 @@ writeVCard (VCard ver attrs) =
 
 readVCards :: SourceName -> B.ByteString -> [VCard]
 readVCards file =
-    map fromAttributes . D.groupByBeginEnd . D.parseDirectory' D.pa_text
+    map fromAttributes . D.groupByBeginEnd . D.parseDirectory' vCardValueParser
 
 parseVersion :: B.ByteString -> Version
 parseVersion s = let (majt, mint) = B.break (=='.') s
@@ -61,6 +60,47 @@ fields s = B.foldr f [B.empty] s
           f '\\' (xs:xss) | Just ('\\',_) <- B.uncons xs = B.cons '\\' xs : xss
           f x (xs:xss) = B.cons x xs : xss
 
+vCardValueParser :: D.ValueParser ExtraValue
+vCardValueParser tps@(typ,params)
+    | Just [valspec] <- D.lookupParameter "value" params = parserFromSpec valspec tps
+    | otherwise = defaultValueParser typ tps
+
+type ValueSpec = B.ByteString
+
+parserFromSpec :: ValueSpec -> D.ValueParser ExtraValue
+parserFromSpec "uri" = D.pa_URI
+parserFromSpec "text" = D.pa_text
+parserFromSpec "date" = D.pa_date
+parserFromSpec "date-time" = D.pa_dateTime
+parserFromSpec "integer" = D.pa_integer
+parserFromSpec "boolean" = D.pa_boolean
+parserFromSpec "float" = D.pa_float
+parserFromSpec "binary" = pa_binary
+parserFromSpec "phone-number" = pa_phoneNumber
+parserFromSpec "utc-offset" = pa_utcOffset
+parserFromSpec "vcard" = pa_subVCard
+
+-- | Maps property types to the corresponding default value parser, in the
+-- absence of any VALUE parameter.
+defaultValueParser :: D.Type -> D.ValueParser ExtraValue
+defaultValueParser typ
+    | typ == D.nakedType "photo" = pa_binary
+    | typ == D.nakedType "bday" = D.pa_date
+    | typ == D.nakedType "adr" = pa_sequence
+    | typ == D.nakedType "tel" = pa_phoneNumber
+    | typ == D.nakedType "tz" = pa_utcOffset
+    | typ == D.nakedType "geo" = pa_sequence
+    | typ == D.nakedType "logo" = pa_binary
+    | typ == D.nakedType "agent" = pa_subVCard
+    | typ == D.nakedType "org" = pa_sequence
+    | typ == D.nakedType "rev" = D.pa_dateTime
+    | typ == D.nakedType "sound" = pa_binary
+    | typ == D.nakedType "url" = D.pa_URI
+    | typ == D.nakedType "key" = pa_binary
+    | otherwise = D.pa_text
+
+-- Parsers for vCard specific value specifications.
+
 -- | A variant of RFC 2425 text type where all ';' characters are escaped
 -- except those that serve as field delimiters.
 pa_sequence :: D.ValueParser ExtraValue
@@ -69,4 +109,21 @@ pa_sequence tps =
     where untxt (D.Text s) = s
 
 pa_binary :: D.ValueParser ExtraValue
-pa_binary _ = return . D.IANAValue . Binary . B.pack . Base64.decode . B.unpack
+pa_binary _ = return . D.IANAValue . Binary
+
+pa_phoneNumber :: D.ValueParser ExtraValue
+pa_phoneNumber tps s =
+    case D.pa_text tps s of [D.Text txt] -> return $ D.IANAValue $ PhoneNumber txt
+                            _ -> error "Expecting single value."
+
+pa_utcOffset :: D.ValueParser ExtraValue
+pa_utcOffset _ s | [sign,h1,h2,':',m1,m2] <- B.unpack s =
+   let hrs = read (h1:h2:[])
+       mins = read (m1:m2:[])
+   in return $ D.IANAValue $ UTCOffset sign hrs mins
+
+pa_subVCard :: D.ValueParser ExtraValue
+pa_subVCard tps =
+    return . D.IANAValue . SubVCard . head . readVCards "<>" . text
+    where text s = case D.pa_text tps s of [D.Text txt] -> txt
+                                           _ -> error "Expecting single value."
