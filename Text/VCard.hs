15bd059c58255298ebdce947ed3768b17dfa4c5c
Mathieu Boespflug <mboes@lix.polytechnique.fr>
2008-11-08 17:58:46 +0100

    Text/VCard.hs documentation.

--- Text/VCard.hs
+++ Text/VCard.hs
@@ -1,3 +1,7 @@
+-- | This module reexports most of the modules in the vCard library in
+-- additionto the mime-directory library, with the exception of
+-- |Text.VCard.Query|, since that library should be imported qualified, as
+-- many of the names clash with standard |Prelude| names.
 module Text.VCard
     ( module D
     , module Text.VCard.Types
