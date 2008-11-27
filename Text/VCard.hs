c356652d44e7c6387be43117a689d96699bf398a
Mathieu Boespflug <mboes@lix.polytechnique.fr>
2008-11-27 12:04:02 +0100

    Replace GNU disclaimers with haddock formatted module headers.

--- Text/VCard.hs
+++ Text/VCard.hs
@@ -1,21 +1,11 @@
--- Copyright (C) 2008 Mathieu Boespflug <mboes@tweag.net>
+-- |
+-- Copyright   : (c) 2008 Mathieu Boespflug
+-- License     : LGPL
+-- Maintainer  : mboes@tweag.net
+-- Stability   : experimental
+-- Portability : non-portable
 --
--- This program is free software; you can redistribute it and/or
--- modify it under the terms of the GNU Lesser General Public License
--- as published by the Free Software Foundation; either version 2
--- of the License, or (at your option) any later version.
---
--- This program is distributed in the hope that it will be useful,
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
--- GNU General Public License for more details.
---
--- You should have received a copy of the GNU General Public License
--- along with this program; if not, write to the Free Software
--- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
--- 02110-1301, USA.
-
--- | This module reexports most of the modules in the vCard library in
+-- This module reexports most of the modules in the vCard library in
 -- additionto the mime-directory library, with the exception of
 -- |Text.VCard.Query|, since that library should be imported qualified, as
 -- many of the names clash with standard |Prelude| names.
